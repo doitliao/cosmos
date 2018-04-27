507e36a1eccc21fd5b5996e0904db4f1c7ae1ea4
liaoyu <liaoyu@whu.edu.cn>
2018-04-27 11:46:25 +0800

    bug

--- code/sorting/src/shell_sort/shell_sort.c
+++ code/sorting/src/shell_sort/shell_sort.c
@@ -30,7 +30,7 @@ int main(void)
   int i;
   int a[10] = { 5 , 211 , 66 , 7 , 12 , 2, 76 , 134 , 99 , 9 };
 
-  printf("In: ")
+  printf("In: ");
   for(i = 0 ; i < 10 ; i++)
   {
     printf("%d ", a[i]);
